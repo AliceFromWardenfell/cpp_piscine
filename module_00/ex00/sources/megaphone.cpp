cafa6150b4556c4c870804d4396119f6bb01a2b7
Alice <purpleplayer@yandex.ru>
2021-09-25 13:08:07 +0300

    replace std::transform

--- module_00/ex00/sources/megaphone.cpp
+++ module_00/ex00/sources/megaphone.cpp
@@ -4,12 +4,12 @@ int main(int argc, char **argv)
 {
     std::string str;
 
-    if (argc > 1)
+	if (argc > 1)
         for (int i = 1; i < argc; i++)
         {
             str = argv[i];
-            std::transform(str.begin(), str.end(), str.begin(), toupper);
-            std::cout << str;
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << (char)toupper(str[j]);
         }
     else
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
